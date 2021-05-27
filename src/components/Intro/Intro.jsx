4b6556b96bc59640824488cd7627f71cefdcffb5
Rohit <vr.rohit03@gmail.com>
2021-05-28 00:14:17 +0530

    front page updated

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,7 +1,21 @@
 import React from "react";
-import "./intro.scss"; 
+import "./intro.scss";
+import { init } from "ityped";
 
 function Intro() {
+
+    const textRef = React.useRef();
+
+    React.useEffect(()=> {
+        init(textRef.current, {
+            showCursor: true,
+            typeSpeed: 80,
+            backDelay: 1500,
+            backSpeed: 60,
+            strings: ["Front-End Developer", "Back-End Developer", "UX/UI Designer"]
+        })
+    }, []);
+
     return (
         <div className="intro" id="intro">
             <div className="left"></div>
@@ -9,11 +23,12 @@ function Intro() {
                 <img src="assets/down-arrow.png" alt="down-arrow" />
             </a>
             <div className="right">
+                <div className="bgsquare1"></div>
                 <div className="wrapper">
-                    <h2>Hello<font color="red">.</font></h2>
+                    <h2>Hello<font color="crimson">.</font></h2>
                     <h2>I am</h2>
                     <h2>Rohit</h2>
-                    <h3><span></span></h3>
+                    <h3>Freelance <span ref={textRef}></span></h3>
                 </div>
             </div>
         </div>
