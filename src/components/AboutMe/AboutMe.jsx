ef40fe33fc98147cff2090f36f67ba3cd1fcb9b9
Rohit <vr.rohit03@gmail.com>
2021-05-30 14:21:21 +0530

    Updated About me page

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -4,6 +4,14 @@ import "./aboutme.scss";
 function AboutMe() {
     return (
         <div className="aboutme" id="aboutme">
+            <div className="container">
+                <div className="item">
+                    <div className="left">
+                        <img src="assets/profile-pic2.png" alt="profile-pic" />
+                    </div>
+                    <div className="right"></div>
+                </div>
+            </div>
         </div>
     );
 }
