a8a82c8ce8c0bf0d3d341a54b379f240e9b7a2a3
Rohit <vr.rohit03@gmail.com>
2021-06-02 14:36:41 +0530

    Updated contact form

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,13 +1,31 @@
 import React from "react";
 import "./contact.scss";
+import Button from '@material-ui/core/Button';
 
 function Contact() {
+
+    const [message, setMessage] = React.useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setMessage(true);
+    }
+
     return (
         <div className="contact" id="contact">
             <h1>CONTACT</h1>
             <h2>_________</h2>
             
-            <div className="top"></div>
+            <div className="top">
+                <form onSubmit={handleSubmit}>
+                    <input type="text" placeholder="Name" autoComplete="new-password" />
+                    <input type="text" placeholder="Enter email" autoComplete="new-password" />
+                    <textarea placeholder="Your Message" autoComplete="new-password" />
+                    {message && <span>Your message was sent successfully. Thanks!</span>}
+                    <button type="submit">SUBMIT</button>
+                </form>
+            </div>
             <div className="bottom"></div>
         </div>
     );
