5f48a3d7d026a68d72b8807f982c747d7b6c0831
Rohit <vr.rohit03@gmail.com>
2021-06-02 18:29:51 +0530

    Contact page additions

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./contact.scss";
-import Button from '@material-ui/core/Button';
+import { FaLinkedinIn, FaInstagram, FaGithub } from 'react-icons/fa';
 
 function Contact() {
 
     const [message, setMessage] = React.useState(false);
+    const year = new Date().getFullYear();
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -26,7 +27,34 @@ function Contact() {
                     <button type="submit">SUBMIT</button>
                 </form>
             </div>
-            <div className="bottom"></div>
+            <div className="bottom">
+                <div className="bottomContainer">
+                    <a
+                    href="https://www.linkedin.com/in/rohit-v-170b76162/">
+                        <button className="socialButton">
+                            <FaLinkedinIn color="white" size="1.5em" />
+                        </button>
+                    </a>
+                    <a
+                    href="https://www.instagram.com/r0hit4real/">
+                        <button className="socialButton">
+                            <FaInstagram color="white" size="1.5em" />
+                        </button>
+                    </a>
+                    <a
+                    href="https://github.com/rohit-vrrr">
+                        <button className="socialButton">
+                            <FaGithub color="white" size="1.5em" />
+                        </button>
+                    </a>
+                </div>
+                <span className="spanName">
+                    ROHIT{" "}
+                    <span className="spanYear">
+                        © {year}
+                    </span>
+                </span>
+            </div>
         </div>
     );
 }
