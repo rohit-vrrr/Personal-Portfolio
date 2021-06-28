12f27fac3117dfa53ca6bb0892600d85f255621f
Rohit <vr.rohit03@gmail.com>
2021-06-28 14:56:29 +0530

    Updated projects data

--- src/data.js
+++ src/data.js
@@ -3,36 +3,36 @@ export const featuredPortfolio = [
         id: 1,
         title: "String",
         img: "assets/js-stringchat.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "A Real Time Chat Application called STRING. The String is an anonymous chat application where you can interact with your friends by creating chat rooms or by joining an existing room.",
+        link: "https://radiant-spire-63204.herokuapp.com/"
     },
     {
         id: 2,
         title: "Keeper App",
         img: "assets/react-keeperapp.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "A clone of the notes keeping application called KEEPER, where you can make notes of anything you wish to note down. Insert the title and content and store the notes.",
+        link: "https://rohit-vrrr.github.io/Keeper-App/"
     },
     {
         id: 3,
         title: "Anonymous.io",
         img: "assets/js-anonymousio.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "A clone of Whisper app using Node.",
+        link: "https://github.com/rohit-vrrr/Anonymous.io"
     },
     {
         id: 4,
         title: "ToDo List",
         img: "assets/react-todolist.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "An initial ToDo list application I had created using React.",
+        link: "https://github.com/rohit-vrrr/ToDo-List-v3"
     },
     {
         id: 5,
         title: "Blog Website",
         img: "assets/js-blog.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "Built a template for the Blog website using EJS. A user can just clone and with minor modifications, the website is good to go.",
+        link: "https://github.com/rohit-vrrr/Blog-Website"
     }
 ];
 
@@ -41,15 +41,15 @@ export const reactPortfolio = [
         id: 1,
         title: "Keeper App",
         img: "assets/react-keeperapp.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "A clone of the notes keeping application called KEEPER, where you can make notes of anything you wish to note down. Insert the title and content and store the notes.",
+        link: "https://rohit-vrrr.github.io/Keeper-App/"
     },
     {
         id: 2,
         title: "ToDo List",
         img: "assets/react-todolist.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "An initial ToDo list application I had created using React.",
+        link: "https://github.com/rohit-vrrr/ToDo-List-v3"
     }
 ];
 
@@ -58,43 +58,43 @@ export const javascriptPortfolio = [
         id: 1,
         title: "String",
         img: "assets/js-stringchat.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "A Real Time Chat Application called STRING. The String is an anonymous chat application where you can interact with your friends by creating chat rooms or by joining an existing room.",
+        link: "https://radiant-spire-63204.herokuapp.com/"
     },
     {
         id: 2,
         title: "Anonymous.io",
         img: "assets/js-anonymousio.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "A clone of Whisper app using Node.",
+        link: "https://github.com/rohit-vrrr/Anonymous.io"
     },
     {
         id: 3,
         title: "Blog Website",
         img: "assets/js-blog.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "Built a template for the Blog website using EJS. A user can just clone and with minor modifications, the website is good to go.",
+        link: "https://github.com/rohit-vrrr/Blog-Website"
     },
     {
         id: 4,
         title: "Newsletter",
         img: "assets/js-newsletter.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "A subscription newsletter application making use of Mailchimp, where the client enters their details and those details are stored in Mailchimp's audiences.",
+        link: "https://whispering-oasis-02139.herokuapp.com/"
     },
     {
         id: 5,
         title: "JWT Authentication",
         img: "assets/js-jwtauth.jpg",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "Generating access tokens and refresh tokens for users when they login using JSON Web Token.",
+        link: "https://github.com/rohit-vrrr/API-Authentication"
     },
     {
         id: 6,
         title: "UpBeat",
         img: "assets/js-upbeat.png",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-        link: "https://www.google.co.in/"
+        desc: "A cool drum set web application called UpBeat. Making use of JavaScript and DOM manipulation.",
+        link: "https://rohit-vrrr.github.io/Drums-UpBeat-Web-Dev/"
     },
 ];
 
