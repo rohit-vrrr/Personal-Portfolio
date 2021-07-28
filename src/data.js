355eacad78a1121919facde80a8502536ed3b6fe
Rohit <vr.rohit03@gmail.com>
2021-07-28 17:44:25 +0530

    Updated portfolio data

--- src/data.js
+++ src/data.js
@@ -1,34 +1,41 @@
 export const featuredPortfolio = [
     {
         id: 1,
+        title: "Memento",
+        img: "assets/react-memento.png",
+        desc: "Using React redux, Node.js, Express, MongoDB, JWT Authentication & OAuth Google login, I built a Full Stack MERN Project. The Web app is called 'Memento' and it is a simple social media MERN application that allows users to post interesting events that happened in their lives.",
+        link: "https://mementowapp.netlify.app/"
+    },
+    {
+        id: 2,
         title: "String",
         img: "assets/js-stringchat.png",
         desc: "A Real Time Chat Application called STRING. The String is an anonymous chat application where you can interact with your friends by creating chat rooms or by joining an existing room.",
         link: "https://radiant-spire-63204.herokuapp.com/"
     },
     {
-        id: 2,
+        id: 3,
         title: "Keeper App",
         img: "assets/react-keeperapp.png",
         desc: "A clone of the notes keeping application called KEEPER, where you can make notes of anything you wish to note down. Insert the title and content and store the notes.",
         link: "https://rohit-vrrr.github.io/Keeper-App/"
     },
     {
-        id: 3,
+        id: 4,
         title: "Anonymous.io",
         img: "assets/js-anonymousio.png",
         desc: "A clone of Whisper app using Node.",
         link: "https://github.com/rohit-vrrr/Anonymous.io"
     },
     {
-        id: 4,
+        id: 5,
         title: "ToDo List",
         img: "assets/react-todolist.png",
         desc: "An initial ToDo list application I had created using React.",
         link: "https://github.com/rohit-vrrr/ToDo-List-v3"
     },
     {
-        id: 5,
+        id: 6,
         title: "Blog Website",
         img: "assets/js-blog.png",
         desc: "Built a template for the Blog website using EJS. A user can just clone and with minor modifications, the website is good to go.",
@@ -39,13 +46,20 @@ export const featuredPortfolio = [
 export const reactPortfolio = [
     {
         id: 1,
+        title: "Memento",
+        img: "assets/react-memento.png",
+        desc: "Using React redux, Node.js, Express, MongoDB, JWT Authentication & OAuth Google login, I built a Full Stack MERN Project. The Web app is called 'Memento' and it is a simple social media MERN application that allows users to post interesting events that happened in their lives.",
+        link: "https://mementowapp.netlify.app/"
+    },
+    {
+        id: 2,
         title: "Keeper App",
         img: "assets/react-keeperapp.png",
         desc: "A clone of the notes keeping application called KEEPER, where you can make notes of anything you wish to note down. Insert the title and content and store the notes.",
         link: "https://rohit-vrrr.github.io/Keeper-App/"
     },
     {
-        id: 2,
+        id: 3,
         title: "ToDo List",
         img: "assets/react-todolist.png",
         desc: "An initial ToDo list application I had created using React.",
